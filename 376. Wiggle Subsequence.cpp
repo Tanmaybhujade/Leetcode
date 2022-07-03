56b4c2a073e1b90f0caddd9b740d9f261354ee19
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2022-07-03 22:02:08 +0530

    Update 376. Wiggle Subsequence.cpp

--- 376. Wiggle Subsequence.cpp
+++ 376. Wiggle Subsequence.cpp
@@ -1,50 +1,6 @@
 class Solution {
 public:
-  // https://leetcode.com/problems/wiggle-subsequence/
-
   int wiggleMaxLength(vector<int> &nums) {
-
-    // Assumptions:
-    // - numbers might not actually differ, so there will be a couple of corner
-    //   cases
-    //
-    // Observations:
-    // - Brute Force:
-    //   -> Generate all possible subsequences O( 2^N )
-    //   -> Count the number of wiggles in each O( N ), saving the longest
-    //   -> O( N 2^N ) time and O( 2^N ) space
-    // - The first wiggle sequence in a vector of length >= 2 will only
-    //   count if the first two elements differ by something other than 0.
-    // - A better DP solution is to use a vector to add up the
-    //   wiggles up until a particular index, i.
-    // - While there might not be a wiggle at index 0, if there is, the
-    //   length sti
-    // - It would be wise to check two different vectors, sort of like
-    //   even / odd, in which case, you expect i + 1 to be greater than i
-    //   and another where you expect i + 1 to be less than i.
-  
-    // - If we take out all of the non-wiggle parts, then the longest wiggle is
-    //   7 elements long    [1,17,5,10,         5,16,8]. There might be other
-    //   wiggles that are 7 elements long  in the vector though. We don't need
-    // 
-    //
-    // Say we have
-    // nums = [ 1,17, 5,10,13,15,10, 5,16, 8]
-    // d    =   [+, -, +, +, +, -, -, +, -]
-    // even = [ 1, 2, 3, 4, 4, 4, 4, 5, 6, 7]
-    // odd  = [ 1, 1, 1, 1, 2, 2, 3, 3, 3, 3]
-    //
-    // So clearly, in this case, the even one wins, because it has
-    // the highest number of wiggles.
-    //
-    // It's probably safe to try both, because we can't assume that the
-    // longest wiggle pattern will start at element 0.
-    //
-    // even[ 0 ] = odd[ 0 ] = nums[ 1 ] - nums[ 0 ] > 0;
-    //
-  
-  
-
     if (nums.empty()) {
       return 0;
     }
