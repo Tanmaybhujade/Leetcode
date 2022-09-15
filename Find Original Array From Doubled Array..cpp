b6a8f8e914c92d5f43f3c8b5ea40f35707cd4ef9
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2022-09-15 18:52:52 +0530

    Create Find Original Array From Doubled Array..cpp

---
+++ Find Original Array From Doubled Array..cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& ch) {
+        int n=ch.size();
+        vector<int>ans;
+        if(n%2)return ans;
+        unordered_map<int,int>mp;
+        for(auto i:ch)mp[i]++;
+        sort(ch.begin(),ch.end());
+        for(int i=0;i<n;i++){
+            if(mp[ch[i]]==0)continue;
+            else{
+                if(mp.find(ch[i]*2)!=mp.end()&&mp[ch[i]*2]){
+                    ans.push_back(ch[i]);mp[ch[i]]--;mp[ch[i]*2]--;
+                }
+                else{
+                   return {};
+                }
+            }
+}
+        return ans;
+    }
+};
