3baad05701865e2ca4fe7106fa560a8fb87907c5
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2022-11-10 19:26:52 +0530

    Update Remove All Adjacent Duplicates In String.java

--- Remove All Adjacent Duplicates In String.java
+++ Remove All Adjacent Duplicates In String.java
@@ -4,7 +4,7 @@ class Solution {
         char ch[]=s.toCharArray();
         for(char c:ch){
             //check if top of stack is equal to this character
-			//and if it so then remove the top element
+			
             if(!st.isEmpty() && st.peek()==c)st.pop();
             else{
                 //add to stack
