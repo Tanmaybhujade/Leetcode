4862e4b59bbe9b997c0603cfba8ca33775569dc2
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2022-11-10 19:24:39 +0530

    Create Remove All Adjacent Duplicates In String.java

---
+++ Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st=new Stack<>();
+        char ch[]=s.toCharArray();
+        for(char c:ch){
+            //check if top of stack is equal to this character
+			//and if it so then remove the top element
+            if(!st.isEmpty() && st.peek()==c)st.pop();
+            else{
+                //add to stack
+                st.push(c);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char ss : st) sb.append(ss);
+        return sb.toString();
+        //while(!st.isEmpty())sb.append(st.pop());
+        //return sb.reverse().toString();
+    }
+}
