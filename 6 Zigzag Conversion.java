62efa0b77c1afcba3cc0d4a1aa66ea4dff0eb69c
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-02-03 19:49:20 +0530

    Create 6 Zigzag Conversion.java

---
+++ 6 Zigzag Conversion.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public String convert(String s, int nRows) {
+        int length=s.length();
+        if(length<2) return s;
+        if(nRows<2) return s;
+		StringBuffer[] buffer=new StringBuffer[nRows];
+		for(int i=0;i<nRows;i++)
+			buffer[i]=new StringBuffer();
+		for(int i=0;i<length;i++){
+			int row=i%(2*nRows-2);
+			if(row<nRows) buffer[row].append(s.charAt(i));
+			else{
+			   buffer[2*nRows-row-2].append(s.charAt(i)); 
+			}
+		}
+		
+		StringBuffer result=new StringBuffer();
+		for(int j=0;j<nRows;j++){
+			result.append(buffer[j]);
+		}
+		return result.toString();
+    }
+}
