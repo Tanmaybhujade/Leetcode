4be1703fb5fec481c59977f97728e932beff0991
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-02-09 20:09:24 +0530

    Create 2306 Naming a Company.java

---
+++ 2306 Naming a Company.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long distinctNames(String[] ideas) {
+        HashSet<String>[] initial_group=new HashSet[26];
+
+        for(int i=0;i<26;i++){
+            initial_group[i]=new HashSet<>();
+
+        }
+
+        for(String idea:ideas){
+            initial_group[idea.charAt(0)-'a'].add(idea.substring(1));
+
+        }
+
+        long count=0;
+        for(int i=0;i<26;i++){
+            for(int j=i+1;j<26;j++){
+                long mutuals=0;
+
+                for(String str:initial_group[i]){
+                    if(initial_group[j].contains(str)){
+                        mutuals++;
+                    }
+
+                }
+
+                count+=2*(initial_group[i].size()-mutuals)*(initial_group[j].size()-mutuals);
+
+            }
+
+        }
+        return count;
+    }
+}
