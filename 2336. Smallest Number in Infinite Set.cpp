05ffec76a848fa0d0c4a4a751c289fe8f702fc40
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-04-25 22:31:22 +0530

    Update 2336. Smallest Number in Infinite Set.cpp

--- 2336. Smallest Number in Infinite Set.cpp
+++ 2336. Smallest Number in Infinite Set.cpp
@@ -1,25 +1,18 @@
-class SmallestInfiniteSet
-{
+
+class SmallestInfiniteSet {
 public:
-    set<int> vec;
-    SmallestInfiniteSet()
-    {
-        vec.clear();
-        for (int i = 1; i <= 1000; i++)
-        {
-            vec.insert(i);
-        }
+    set<int> st;
+    SmallestInfiniteSet() {
+        for(int i=1;i<=1000;i++) st.insert(i);
     }
-
-    int popSmallest()
-    {
-        int t = *vec.begin();
-        vec.erase(vec.begin());
-        return t;
+    
+    int popSmallest() {
+        int ans = *st.begin();
+        st.erase(ans);
+        return ans;
     }
-
-    void addBack(int num)
-    {
-        vec.insert(num);
+    
+    void addBack(int num) {
+        st.insert(num);
     }
 };
