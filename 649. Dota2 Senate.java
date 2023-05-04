cafecf344dfe73baba3fe49e17259104809b0ae9
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-05-04 21:04:11 +0530

    Create 649. Dota2 Senate.java

---
+++ 649. Dota2 Senate.java
@@ -0,0 +1,26 @@
+class Solution {
+
+public String predictPartyVictory(String s)
+
+{
+    int k=s.length();
+        for(int i=1;i<s.length();i++)
+        {  char c=s.charAt(0);
+               if(c!=s.charAt(i))
+               {
+                   
+                       if(i+1<s.length())
+                                s=s.substring(1,i)+s.substring(i+1);
+                       else
+                                s=s.substring(1,i);
+                       s+=c;
+                       i-=2;
+               }
+         // System.out.println(s);
+        }
+   if(s.charAt(0)=='R')
+           return "Radiant";
+        else
+                return "Dire";
+}
+}
