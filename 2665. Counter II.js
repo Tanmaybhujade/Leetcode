74b4be279daf7ce5ffc8997e569430ab10cc108b
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-05-07 20:12:20 +0530

    Create 2665. Counter II.js

---
+++ 2665. Counter II.js
@@ -0,0 +1,13 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let value = init;
+
+    return {
+        increment: () => ++value,
+        decrement: () => --value,
+        reset: () => value = init,
+    }
+};
