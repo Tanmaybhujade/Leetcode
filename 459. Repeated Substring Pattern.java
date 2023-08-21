eb038bd74011f3fcac59a183dc97ffef18ae3a81
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-08-21 19:11:23 +0530

    Create 459. Repeated Substring Pattern.java

---
+++ 459. Repeated Substring Pattern.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+
+        for (int i = len / 2; i > 0; i--) {
+            if (len % i == 0 && s.charAt(i - 1) == s.charAt(len - 1)) {
+                String ss = s.substring(0, i);
+                int repeat = len / i;
+
+                if (ss.repeat(repeat).equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
