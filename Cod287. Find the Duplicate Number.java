4db6e2c4df0f65adac8a0efdab30e01a4a224c96
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2023-09-19 19:39:06 +0530

    Create  Cod287. Find the Duplicate Number.java

---
+++ Cod287. Find the Duplicate Number.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        // Initialize slow and fast pointers
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        // Phase 1: Find the intersection point in the cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        // Phase 2: Find the entrance to the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        // Return the duplicate number
+        return slow;
+    }
+}
