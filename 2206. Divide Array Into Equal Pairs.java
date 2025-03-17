1f1f42de200e53c6bc8cee44f45f38b066854cde
TANMAY BHUJADE <80476913+Tanmaybhujade@users.noreply.github.com>
2025-03-17 13:46:04 +0530

    Create 2206. Divide Array Into Equal Pairs.java

---
+++ 2206. Divide Array Into Equal Pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        for (int count : freqMap.values()) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
